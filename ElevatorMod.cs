bbf63e0755371c7fa5c2191df57b23d4795e0ade
Colata <sammitchell246@gmail.com>
2015-07-09 17:48:46 +1000

    Elevator Mod V1.003
    Elevator Mod V1.003
    -Added markers for elevator trigger points

--- ElevatorMod.cs
+++ ElevatorMod.cs
@@ -85,6 +85,26 @@ public class Main : Script
 
     private void OnTick(object sender, EventArgs e)
     {
+        //Markers
+        //Maze Bank Markers
+        Function.Call(Hash.DRAW_MARKER, 2, -59.82701f, -790.3538f, 44.22732f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, -66.43941f, -822.1783f, 321.2873f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        //Union Depository Markers
+        Function.Call(Hash.DRAW_MARKER, 2, 6.295846f, -709.3442f, 45.97305f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, -2.279792f, -690.0576f, 250.4136f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        //IAA Building Markers
+        Function.Call(Hash.DRAW_MARKER, 2, 105.3145f, -625.6478f, 44.22019f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, 108.269f, -640.4637f, 258.1489f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        //FIB Building Markers
+        Function.Call(Hash.DRAW_MARKER, 2, 99.49534f, -743.5652f, 45.75476f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, 132.4646f, -726.3422f, 258.1525f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        //Half Constructed Building Markers
+        Function.Call(Hash.DRAW_MARKER, 2, -159.6062f, -944.0149f, 269.218f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, -184.1684f, -1016.074f, 30.07096f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        //Hopsital1 Markers
+        Function.Call(Hash.DRAW_MARKER, 2, 360.2171f, -584.9374f, 28.8215f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+        Function.Call(Hash.DRAW_MARKER, 2, 340.2888f, -584.173f, 74.1657f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+
         //BOTTOM
         float MazeBankElevatorDistanceBOTTOM = GTA.World.GetDistance(player.Position, mazeBankElevatorBOTTOM);
         float unionElevatorDistanceBOTTOM = GTA.World.GetDistance(player.Position, unionElevatorBOTTOM);
