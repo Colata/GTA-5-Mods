e0ee379ba3e5941148ba032e9797d5bb8784935d
Colata <sammitchell246@gmail.com>
2015-07-11 01:21:55 +1000

    Elevator Mod V1.006
    Elevator Mod V1.006
    -This update refractured the code and split the individual elevators
    into classes for ease of editing

---
+++ BadgerCenter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using Menu = GTA.Menu;
+using System.IO;
+
+public class BadgerCenter : Script
+{
+    Ped player = Game.Player.Character;
+
+    public BadgerCenter()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+
+        Interval = 10;
+
+        Blip BadgerCenterBlip = World.CreateBlip(badgerCenterElevatorBOTTOM, 2);
+        {
+            BadgerCenterBlip.Sprite = BlipSprite.Helicopter;
+            BadgerCenterBlip.Scale = 75f;
+        }
+    }
+
+    //BOTTOM Radius
+    bool badgerCenterRadiusBOTTOM = false;
+
+    //TOP Radius
+    bool badgerCenterRadiusTOP = false;
+
+    //TOP
+    GTA.Math.Vector3 badgerCenterElevatorBOTTOM = new GTA.Math.Vector3(478.8547f, -107.6386f, 63.1579f);
+
+    //BOTTOM
+    GTA.Math.Vector3 badgerCenterElevatorTOP = new GTA.Math.Vector3(469.7626f, -107.6571f, 117.6346f);
+
+
+    void OnTick(object sender, EventArgs e)
+    {
+        //DISTANCE BOTTOM
+        float badgerCenterDistanceBOTTOM = GTA.World.GetDistance(player.Position, badgerCenterElevatorBOTTOM);
+        //DISTANCE TOP
+        float badgerCenterDistanceTOP = GTA.World.GetDistance(player.Position, badgerCenterElevatorTOP);
+
+        //ACTIVATE BOTTOM
+        if (badgerCenterDistanceBOTTOM < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the top of the Hospital");
+            badgerCenterRadiusBOTTOM = true;
+        }
+        //ACTIVATE TOP
+        if (badgerCenterDistanceTOP < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the bottom of the Hospital");
+            badgerCenterRadiusTOP = true;
+        }
+    }
+
+    public void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        //TOP TELEPORT
+        if (badgerCenterRadiusBOTTOM && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = badgerCenterElevatorTOP; //TOP CORDS
+            }
+        }
+        //BOTTOM TELEPORT
+        if (badgerCenterRadiusTOP && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = badgerCenterElevatorBOTTOM; //BOTTOM CORDS
+            }
+        }
+    }
+}
---
+++ IAA.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using Menu = GTA.Menu;
+using System.IO;
+
+public class IAA : Script
+{
+    Ped player = Game.Player.Character;
+
+    public IAA()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+
+        Interval = 10;
+
+        Blip IAABlip = World.CreateBlip(iaaElevatorBOTTOM, 2);
+        {
+            IAABlip.Sprite = BlipSprite.Helicopter;
+            IAABlip.Scale = 75f;
+        }
+    }
+
+    //BOTTOM Radius
+    bool iaaRadiusBOTTOM = false;
+
+    //TOP Radius
+    bool iaaRadiusTOP = false;
+
+    //TOP
+    GTA.Math.Vector3 iaaElevatorBOTTOM = new GTA.Math.Vector3(105.3145f, -625.6478f, 44.22019f);
+
+    //BOTTOM
+    GTA.Math.Vector3 iaaElevatorTOP = new GTA.Math.Vector3(108.269f, -640.4637f, 258.1489f);
+
+
+    void OnTick(object sender, EventArgs e)
+    {
+        //DISTANCE BOTTOM
+        float iaaElevatorDistanceBOTTOM = GTA.World.GetDistance(player.Position, iaaElevatorBOTTOM);
+        //DISTANCE TOP
+        float iaaElevatorDistanceTOP = GTA.World.GetDistance(player.Position, iaaElevatorTOP);
+
+        //ACTIVATE BOTTOM
+        if (iaaElevatorDistanceBOTTOM < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the top of the IAA Building");
+            iaaRadiusBOTTOM = true;
+        }
+        //ACTIVATE TOP
+        if (iaaElevatorDistanceTOP < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the bottom of the IAA Building");
+            iaaRadiusTOP = true;
+        }
+    }
+
+    public void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        //TOP TELEPORT
+        if (iaaRadiusBOTTOM && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = iaaElevatorTOP; //TOP CORDS
+            }
+        }
+        //BOTTOM TELEPORT
+        if (iaaRadiusTOP && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = iaaElevatorBOTTOM; //BOTTOM CORDS
+            }
+        }
+    }
+}
