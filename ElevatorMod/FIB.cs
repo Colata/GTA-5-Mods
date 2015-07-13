63357fceece71c1916a15ffa8370bc687cb751d2
Colata <sammitchell246@gmail.com>
2015-07-13 21:52:27 +1000

    Elevator Mod
    Elevator Mod

---
+++ ElevatorMod/FIB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using Menu = GTA.Menu;
+using System.IO;
+
+public class FIB : Script
+{
+    Ped player = Game.Player.Character;
+
+    public FIB()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+
+        Interval = 10;
+  
+        Blip FIBBlip = World.CreateBlip(fibElevatorBOTTOM, 2);
+        {
+            FIBBlip.Sprite = BlipSprite.Helicopter;
+            FIBBlip.Scale = 75f;
+        }
+    }
+
+    //BOTTOM Radius
+    bool fibRadiusBOTTOM = false;
+
+    //TOP Radius
+    bool fibRadiusTOP = false;
+
+    //TOP
+    GTA.Math.Vector3 fibElevatorBOTTOM = new GTA.Math.Vector3(99.49534f, -743.5652f, 45.75476f);
+
+    //BOTTOM
+    GTA.Math.Vector3 fibElevatorTOP = new GTA.Math.Vector3(132.4646f, -726.3422f, 258.1525f);
+
+
+    void OnTick(object sender, EventArgs e)
+    {
+        //DISTANCE BOTTOM
+        float fibElevatorDistanceBOTTOM = GTA.World.GetDistance(player.Position, fibElevatorBOTTOM);
+        //DISTANCE TOP
+        float fibElevatorDistanceTOP = GTA.World.GetDistance(player.Position, fibElevatorTOP);
+
+        //ACTIVATE BOTTOM
+        if (fibElevatorDistanceBOTTOM < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the top of the FIB Building");
+            fibRadiusBOTTOM = true;
+        }
+        //ACTIVATE TOP
+        if (fibElevatorDistanceTOP < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the bottom of the FIB Building");
+            fibRadiusTOP = true;
+        }
+    }
+
+    public void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        //TOP TELEPORT
+        if (fibRadiusBOTTOM && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = fibElevatorTOP; //TOP CORDS
+            }
+        }
+        //BOTTOM TELEPORT
+        if (fibRadiusTOP && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = fibElevatorBOTTOM; //BOTTOM CORDS
+            }
+        }
+    }
+}
---
+++ ElevatorMod/UnionDepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using Menu = GTA.Menu;
+using System.IO;
+
+public class UnionDepository : Script
+{
+    Ped player = Game.Player.Character;
+
+    public UnionDepository()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+
+        Interval = 10;
+
+        Blip UnionBlip = World.CreateBlip(unionElevatorBOTTOM, 2);
+        {
+            UnionBlip.Sprite = BlipSprite.Helicopter;
+            UnionBlip.Scale = 75f;
+        }
+    }
+
+    //BOTTOM Radius
+    bool unionRadiusBOTTOM = false;
+
+    //TOP Radius
+    bool unionRadiusTOP = false;
+
+    //TOP
+    GTA.Math.Vector3 unionElevatorBOTTOM = new GTA.Math.Vector3(6.295846f, -709.3442f, 45.97305f);
+
+    //BOTTOM
+    GTA.Math.Vector3 unionElevatorTOP = new GTA.Math.Vector3(-2.279792f, -690.0576f, 250.4136f);
+
+
+    void OnTick(object sender, EventArgs e)
+    {
+        //DISTANCE BOTTOM
+        float unionElevatorDistanceBOTTOM = GTA.World.GetDistance(player.Position, unionElevatorBOTTOM);
+        //DISTANCE TOP
+        float unionElevatorDistanceTOP = GTA.World.GetDistance(player.Position, unionElevatorTOP);
+
+        //ACTIVATE BOTTOM
+        if (unionElevatorDistanceBOTTOM < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the top of the Union Depository");
+            unionRadiusBOTTOM = true;
+        }
+        //ACTIVATE TOP
+        if (unionElevatorDistanceTOP < 2f && !player.IsInVehicle())
+        {
+            UI.ShowSubtitle("Press F to travel to the bottom of the Union Depository");
+            unionRadiusTOP = true;
+        }
+    }
+
+    public void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        //TOP TELEPORT
+        if (unionRadiusBOTTOM && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = unionElevatorTOP; //TOP CORDS
+            }
+        }
+        //BOTTOM TELEPORT
+        if (unionRadiusTOP && !player.IsInVehicle())
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                Game.Player.Character.Position = unionElevatorBOTTOM; //BOTTOM CORDS
+            }
+        }
+    }
+}
